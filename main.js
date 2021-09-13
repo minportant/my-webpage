64552becb9d97f556d68d4e1f75b2736a3bfe22b
Min Woo Kim <minportant@gmail.com>
2021-09-13 16:58:19 -0400

    added content

--- main.js
+++ main.js
@@ -11,7 +11,50 @@ $(document).ready(function() {
 
     AOS.init({
         easing: 'ease',
-        duration: 1800,
-        once: true
+        duration: 1800
     });
 });
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 5000);
+
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow2');
+}, 5000);
+
+$("#slideshow3 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow3 > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow3');
+}, 5000);
+
+$("#slideshow4 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow4 > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow4');
+}, 5000);
